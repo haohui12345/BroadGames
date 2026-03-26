78d96546504f832c2361d713fb316740be89204f
Le Nguyen Quoc Toan <toanlenguyen1705@gmail.com>
2026-03-27 00:56:25 +0700

    update getmyranking feature

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getProfile, updateProfile, searchUsers, getUserById, getRankings, getFriendRankings, getPersonalRankings } = require('../controllers/userController');
+const { getProfile, updateProfile, searchUsers, getUserById, getRankings, getFriendRankings, getMyRankings } = require('../controllers/userController');
 const { authenticateToken } = require('../middlewares/auth');
 
 router.use(authenticateToken)
@@ -10,7 +10,7 @@ router.put('/profile', updateProfile);
 router.get('/search', searchUsers);
 router.get('/rankings', getRankings);
 router.get('/rankings/friends', getFriendRankings);
-router.get('/rankings/personal', getPersonalRankings);
+router.get('/rankings/me', getMyRankings);
 router.get('/:id', getUserById);
 
 module.exports = router;
